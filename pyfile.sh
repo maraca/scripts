2a662abb1c15c320789d335af7d4462b76e7d2a7
Martin Cozzi <martin@orchid.formspring.me>
2011-01-13 15:14:33 -0800

    Changing order of variable for better use

--- pyfile.sh
+++ pyfile.sh
@@ -2,16 +2,16 @@
 
 python_body=$(cat <<EOF
 #!/usr/bin/python2.6
-# Copyright (c) 2011, ${2}.
+# Copyright (c) 2011, ${1}.
 """Enter brief description here.
 
 Enter more details here about this file.
 """
 
-__author__ = '${3}'
+__author__ = '${2}'
 
 if __name__ == '__main__':
 EOF
 )
 
-echo -e "$python_body" > "${1}"
+echo -e "$python_body" > "${3}"
